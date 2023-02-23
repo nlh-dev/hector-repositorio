8e12f00481dcd5e8d0ae76cc2c1cbf1c9fcb3c7b
nlh-dev <hnavarro331@gmail.com>
2023-02-22 22:03:58 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,93 @@
+// console.log ("Hola Mundo");
+
+// let n1 = 2;
+// let n2 = 3;
+
+// var suma = n1 + n2;
+
+// console.log(suma)
+
+// let n3;
+// let n4;
+
+// n4 = prompt("Digite un Numero: ");
+
+// if(n4 % 2 == 0)
+//     {
+//     console.log("El Numero " + n4 + " Suministrado es Par!");
+//     }
+// else
+//     {
+//     console.log("El Numero Suministrado es Impar!");
+//     }
+
+//  let a = "3";
+
+//  if (a === n3){
+//     console.log(true);
+//  }
+
+
+
+
+// const constante = () => {
+//     console.log("Hola Mundo") 
+// }
+
+// constante();
+
+//  function imprimir (n6){
+//     console.log("tu numero es: " + n6);
+//  }
+
+//  (n4 % 2 == 0) ? imprimir(n4) : console.log("Es Impar!")
+
+// function suma (a, b){
+//     let resultado = a + b;
+//     console.log("LA Suma es: " + (resultado));
+// }
+
+// let a = parseInt(prompt ("Digite un Numero"));
+// let b = parseInt(prompt ("Digite otro Numero"));
+
+// suma(a, b);
+
+// document.write("Hola Zurdo");
+
+// let v = document.getElementById("class1")
+
+// v.innerHTML += "Hola Mundo+";
+
+// // console.log(`zurdo ${v.innerHTML}`)
+
+// let list = document.getElementById('list');
+
+// let tarea = document.getElementById('tarea');
+
+// function todo (){
+//     list.innerHTML += `<p ${tarea.value} </p>`;
+//     tarea.value = "";
+// }
+
+
+// a = true
+
+// function toogle(){
+//     let light = document.getElementById("light");
+
+//     if(a == true){
+//         light.style.background = "#333" ;
+//         a = false
+//         console.log(a)
+
+//     }
+//     else  {
+//         light.style.background = "#ddd";
+//         a = true
+//         console.log(a)
+//     }
+// };
+
+
+
+console.log('Hello world!');
